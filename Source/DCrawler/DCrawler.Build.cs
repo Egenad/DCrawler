0c17846bff3c6d93b624869bbb4a18088f9924f1
Angel Terol <egenad8@gmail.com>
2023-03-07 19:10:29 +0100

    BugSplat

--- Source/DCrawler/DCrawler.Build.cs
+++ Source/DCrawler/DCrawler.Build.cs
@@ -8,6 +8,6 @@ public class DCrawler : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UM"G });
 	}
 }
